7e6a65d2b27744d952b6f39188482a92e65fbb3b
hero <hero@heroui-MacBookPro.local>
2021-03-06 20:24:50 +0900

    add Hello World

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "leaning_scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/leanrning/scala/chapter01/chapter01.scala
@@ -0,0 +1,8 @@
+package leanrning.scala.chapter01
+
+object chapter01 {
+  def main(args: Array[String]) {
+    print("Hello, World")
+
+  }
+}
