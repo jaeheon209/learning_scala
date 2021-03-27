cc5609c885b775d8455d3bcc255af5c4203a8a29
jacob-branch <jacob.branch@kakaocorp.com>
2021-03-27 16:46:29 +0900

    Add chapter01

--- src/main/scala/leanrning/scala/chapter01/chapter01.scala
+++ src/main/scala/leanrning/scala/chapter01/chapter01.scala
@@ -2,7 +2,43 @@ package leanrning.scala.chapter01
 
 object chapter01 {
   def main(args: Array[String]) {
+
+    //P6
     print("Hello, World")
+
+    //TIP IntelliJ Scala REPL shortcut : cmd + shift + D
+
+    /* REPL
+    scala> 5 * 7
+    val res0: Int = 35
+
+    scala> 2 * res0
+    val res1: Int = 70
+    */
+
+    //P7 연습 문제
+
+    //2번
+    /*
+    scala> def  cToF(x : Double) = (x * 9/5) + 32
+    def cToF(x: Double): Double
+
+    scala> cToF(22.5)
+    val res0: Double = 72.5
+
+    scala> (res0-32)/9*5
+    val res1: Double = 22.5
+     */
+
+    //3번
+    // :load <file>
+
+    //4번
+    // :paste 입력 후 scala 소스 붙여 넣기
+    // 붙여 놓기 모드 나가면 실행
+
+
+
 
   }
 }
