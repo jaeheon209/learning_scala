5d3913866239ff8002e90662f5db1fe802bee0ba
jacob-branch <jacob.branch@kakaocorp.com>
2021-04-02 13:59:30 +0900

    refactor rename chapter

--- src/main/scala/leanrning/scala/chapter01/chapter02.scala
+++ src/main/scala/leanrning/scala/chapter01/chapter02.scala
@@ -1,130 +1,44 @@
 package leanrning.scala.chapter01
 
 object chapter02 {
+  def main(args: Array[String]) {
 
-  //P10
-  /*
-    scala> val x: Int = 5
-    val x: Int = 5
+    //P6
+    print("Hello, World")
 
-    scala> x
-    val res0: Int = 5
+    //TIP IntelliJ Scala REPL shortcut : cmd + shift + D
 
-    scala> x * 2
-    val res1: Int = 10
+    /* REPL
+    scala> 5 * 7
+    val res0: Int = 35
 
-    scala> x / 5
-    val res2: Int = 1
+    scala> 2 * res0
+    val res1: Int = 70
+    */
 
-    scala> res0 * res1
-    val res3: Int = 50
+    //P7 연습 문제
 
-   */
+    //2번
+    /*
+    scala> def  cToF(x : Double) = (x * 9/5) + 32
+    def cToF(x: Double): Double
 
-  //P17
-  /*
-   1) small -> big is ok
+    scala> cToF(22.5)
+    val res0: Double = 72.5
 
-    scala> val b: Byte = 10
-    val b: Byte = 10
+    scala> (res0-32)/9*5
+    val res1: Double = 22.5
+     */
 
-    scala> val s: Short = b
-    val s: Short = 10
+    //3번
+    // :load <file>
 
-    scala> val d: Double = s
-    val d: Double = 10.0
-
-   2)  big -> small is X
-    scala> val l: Long = 20
-    val l: Long = 20
-
-    scala> val i: Int = l
-                        ^
-           error: type mismatch;
-            found   : Long
-            required: Int
-
-    scala> val i: Int = l.toInt
-    val i: Int = 20
-   */
-
-  def main(args: Array[String]): Unit = {
-    //P20
-    val greeting = "Hello, " + "World"
-    println(greeting) // Hello. World
-
-    val matched = (greeting == "Hello, World")
-    println(matched) // true
-
-    val theme = "Na " * 5 + "Batman!"
-    println(theme) //  Na Na Na Na Na Batman!
-
-    // 파이픈이 안먹는다 |...
-    val greeting2 =
-      """She suggested reformatting the file
-by replaceing tabs (\t) with newlines (\n);
-"Why do that?", he asked."""
-    println(greeting2)
-
-    val approx = 355 / 113f
-    println(approx)
-    println("Pi, using 355/113, is about " + approx + ".")
-    println(s"Pi, using 355/113, is about $approx.")
-
-    val item = "apple"
-    println(s"How do you like them ${item}s?")
-    println(s"Fish n chips n vinegar, ${"pepper " * 3}salt")
-
-    println(f"I wrote a new $item%.3s today")
-    println(f"Enjoying this $item ${355 / 113.0}%.5f times today")
-
-    //matches
-    val matches_ex = "Froggy went a' courting" matches ".*courting"
-    println(matches_ex) //true
-
-    val replaceAll_ex = "milk, tea, muck" replaceAll("m[^ ]+k", "coffee")
-    println(replaceAll_ex) // m??k to coffee
-
-    val replaceFirst_ex = "milk, tea, muck" replaceFirst("m[^ ]+k", "coffee")
-    println(replaceFirst_ex) // first
+    //4번
+    // :paste 입력 후 scala 소스 붙여 넣기
+    // 붙여 놓기 모드 나가면 실행
 
 
-    val input = "Enjoying this apple 3.14159 times today"
-    val pattern = """.*apple ([\d.]+) times .*""".r
-    val pattern(amountText) = input
-    val amount = amountText.toString
-    println(amount)
 
-    //ASC! Char
-    val c = 'A'
-    val i: Int = c
-    val t: Char = 116
-    println(s"c : $c, i: $i, t: $t")
-
-    val info = (5, "Korben", true)
-    println(info)
-    println(info._2) //Korben
-
-    val red = "red" -> "0xff0000"
-    println(red)
-
-    val reversed = red._2 -> red._1
-    println(reversed)
-
-    //연습문제
-    // 일반적은 escape는 \을 사용 $의 경우는 $$ 2개를 사용
-    val money = 2.7255
-    println(s"You own $$${money}")
-
-    val a = 128
-//    val b: Char = a
-//    val c: String = a
-    val d: Double = a
-    val d2: Int = d.toInt
-
-    println(f"$a, $d, $d2")
 
   }
-
-
 }
