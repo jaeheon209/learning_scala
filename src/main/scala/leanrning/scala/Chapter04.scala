d24980bb11bd2f534df4b23712c9228edf51cd97
jacob-branch <jacob.branch@kakaocorp.com>
2021-04-05 21:57:17 +0900

    Update Chapter04

--- src/main/scala/leanrning/scala/Chapter04.scala
+++ src/main/scala/leanrning/scala/Chapter04.scala
@@ -76,22 +76,82 @@ object Chapter04 {
 
     println(max(42, 181, 19))
 
-
-
-
     // 6.Calling Functions WIth Named Parameters
+    def greet(prefix: String, name: String) = s"$prefix $name"
+
+    val greeting1 = greet("Ms", "Brown")
+    println(greeting1)
+
+    val greeting2 = greet(name = "Red", prefix = "Mr")
+    println(greeting2)
 
     // 7.Parameters With Default Values
+    def greetDefaultPrefix(prefix: String = "", name: String) = s"$prefix $name"
+
+    val greeting3 = greetDefaultPrefix(name = "Paul")
+    println(greeting3)
+
+    def greetDefaultName(prefix: String, name: String = "") = s"$prefix $name"
+
+    val greeting4 = greetDefaultName("Mr")
+    println(greeting4)
 
     // 8.VarArg Parameters
+    def sum(items: Int*): Int = {
+      var total = 0
+      for (i <- items) total += i
+      total
+    }
+
+    val sumEx1 = sum(10, 20, 30)
+    println(f"sum : ${sumEx1}")
+
+    val sumEx2 = sum()
+    println(f"sum : ${sumEx2}")
+
 
     // 9. Parameter Groups
+    def max2(x: Int)(y: Int) = if (x > y) x else y
 
-    // 10. Type Parametersw
+    val larger = max2(20)(39)
+    println(f"larger : ${larger}")
+
+
+    // 10. Type Parameters
+    def identityuString(i: String): String = i
+
+    def identityuInt(i: Int): Int = i
+
+    /* ERROR Code
+    def identity(a: Any): Any = a
+    val s: String = identity("Hello")
+     */
+    //use!
+    def identity[A](a: A): A = a
+
+    val s: String = identity[String]("Hello")
+    val d: Double = identity[Double](2.717)
+
+    print(f"identity test | String : $s, Double : $d")
+
+    val s1: String = identity("Hello")
+    val d1: Double = identity(2.717)
+
+    val s2 = identity("Hello")
+    val d2 = identity(2.717)
+
 
     // 11. Methods ad  Operators
+    val jpg = "vacation.jpg"
+    val isJPEG = jpg.endsWith(".jpg")
+    println(f"$jpg is $isJPEG")
 
-    // 12. Writing Reabdfable Functions
+
+    // 12. Writing Readable Functions
+    def safeTrim2(s: String): String = {
+      if (s == null) return null
+      s.trim
+    }
 
 
   }
